9904ba2b01b3469945f7916159927de7aad6d716
de1ux <nathan@de1ux.com>
2021-08-11 15:24:48 -0600

    Argon demo

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const {Client} = require('pg');
 
 const sentry = require("@sentry/node");
 const tracing = require("@sentry/tracing");
+const crypto = require("crypto");
+const argon2 = require('argon2');
 
 let db;
 if (process.env.DATABASE_URL) {
@@ -24,13 +26,35 @@ if (process.env.DATABASE_URL) {
 
 db.connect();
 
-const createItemTable = async () => {
+const createTables = async () => {
     await db.query(`
     CREATE TABLE IF NOT EXISTS items (
         name TEXT PRIMARY KEY,
         price INT
     );
+
+    DROP TABLE IF EXISTS users;
+    CREATE TABLE IF NOT EXISTS users (
+        username TEXT PRIMARY KEY,
+        hash TEXT
+    );
   `);
+}
+
+const getHashByUsername = async (username) => {
+    const users = await db.query(`
+        SELECT * FROM users WHERE username=$1 LIMIT 1
+    `, [username])
+    if (users.rowCount === 0) {
+        return null
+    }
+    return users.rows[0].hash;
+}
+
+const insertUser = async (username, hash) => {
+    await db.query(`
+        INSERT INTO users VALUES($1, $2);
+    `, [username, hash])
 }
 
 const getItems = async () => {
@@ -39,7 +63,7 @@ const getItems = async () => {
 }
 
 const createServer = () => {
-    createItemTable();
+    createTables();
 
     const app = express()
 
@@ -65,11 +89,35 @@ const createServer = () => {
 
     app.get('/items', async (req, res) => {
         res.send({items: await getItems()})
-    })
+    });
 
     app.get('/error', (req, res) => {
         throw new Error("Raising an error!");
-    })
+    });
+
+
+    app.post('/signup', async (req, res) => {
+        const salt = await crypto.randomBytes(128)
+
+        const hash = await argon2.hash(req.body['password'], {
+            salt: salt,
+            hashLength: 128,
+            parallelism: 4,
+            timeCost: 6,
+        });
+        await insertUser(req.body['username'], hash);
+
+        res.send({result: "ok"});
+    });
+
+    app.post('/login', async (req, res) => {
+        const hash = await getHashByUsername(req.body['username']);
+        console.log(hash);
+
+        const valid = await argon2.verify(hash, Buffer.from(req.body['password']));
+
+        res.send({loggedIn: valid})
+    });
 
     app.use(sentry.Handlers.errorHandler());
 
