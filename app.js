32f7c16f15a5b5d864bdfa6249f642db7f30fe34
de1ux <nathan@de1ux.com>
2021-08-11 18:43:35 -0600

    Password hashing

--- app.js
+++ app.js
@@ -4,6 +4,39 @@ const {Client} = require('pg');
 
 const sentry = require("@sentry/node");
 const tracing = require("@sentry/tracing");
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
+const axios = require("axios");
+const jwtAuthz = require("express-jwt-authz");
+const argon2 = require("argon2");
+const crypto = require("crypto");
+
+const cache = {}
+
+const auth0Domain = 'https://dev-lifxamti.auth0.com';
+
+const checkJwt = jwt({
+    secret: jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `${auth0Domain}/.well-known/jwks.json`
+    }),
+
+    audience: `https://nathans-backend.herokuapp.com/`,
+    issuer: [`${auth0Domain}/`],
+    algorithms: ['RS256']
+});
+
+
+const checkPermissions = (permissions) => {
+    return jwtAuthz(permissions, {
+        customScopeKey: "permissions",
+        checkAllScopes: true,
+        failWithError: true
+    });
+};
+
 
 let db;
 if (process.env.DATABASE_URL) {
@@ -24,13 +57,32 @@ if (process.env.DATABASE_URL) {
 
 db.connect();
 
-const createItemTable = async () => {
+const createTables = async () => {
     await db.query(`
     CREATE TABLE IF NOT EXISTS items (
         name TEXT PRIMARY KEY,
         price INT
     );
+    
+    CREATE TABLE IF NOT EXISTS users (
+        username TEXT PRIMARY KEY, 
+        hash TEXT    
+   )
   `);
+}
+
+const createUser = async (username, password) => {
+    await db.query(`
+        INSERT INTO users VALUES($1, $2);
+    `, [username, password])
+}
+
+const getHashByUsername = async (username) => {
+    const users = await db.query(`
+        SELECT hash FROM users WHERE username=$1 LIMIT 1;
+    `, [username])
+
+    return users.rows[0].hash
 }
 
 const getItems = async () => {
@@ -38,8 +90,26 @@ const getItems = async () => {
     return items.rows;
 }
 
+const getUserData = async (req) => {
+    const accessToken = req.header('Authorization');
+    if (cache[accessToken]) {
+        return cache[accessToken]
+    }
+
+    const userInfo = await axios.get(`${auth0Domain}/userinfo`, {
+        headers: {
+            "Authorization": accessToken
+        }
+    }).then(d => d.data);
+
+    cache[accessToken] = userInfo
+
+    return userInfo
+
+}
+
 const createServer = () => {
-    createItemTable();
+    createTables();
 
     const app = express()
 
@@ -62,16 +132,36 @@ const createServer = () => {
     app.use(express.json())
     app.use(cors());
 
+    app.post("/signup", async (req, res) => {
+        const username = req.body['username'];
+        const password = req.body['password'];
 
-    app.get('/items', async (req, res) => {
-        res.send({items: await getItems()})
+        const salt = await crypto.randomBytes(32)
+        const hash = await argon2.hash(password, {
+            salt: salt
+        })
+
+        await createUser(username, hash);
+
+        res.send({'result': 'ok'});
     })
 
-    app.get('/error', (req, res) => {
-        throw new Error("Raising an error!");
-    })
+    app.post("/login", async (req, res) => {
+        const username = req.body['username'];
+        const password = req.body['password'];
+
+        const hash = await getHashByUsername(username)
+        const isValid = await argon2.verify(hash, Buffer.from(password))
+
+        res.send({loggedIn: isValid});
+    });
 
     app.use(sentry.Handlers.errorHandler());
+
+    app.use((err, req, res, next) => {
+        console.error(err)
+        next(err)
+    });
 
     return app
 };
